01218d7d76df4aef0af051b6f3fb98f8987f0b0c
György <gyorgy_kiraly@yahoo.com>
2020-05-04 15:00:25 +0200

    Wait for restaring ssh daemon

--- prepareDevServer.sh
+++ prepareDevServer.sh
@@ -11,41 +11,40 @@
 # add the following line to the sudoers file
 # User privilege specification
 
-sudo sed -i '/User privilege specification/a gkiraly  ALL\=\(ALL\) ALL' /etc/sudoers
-#sudo sed -Ei 's/^#(User privilege specification.*)/a gkiraly  ALL=(ALL) ALL' /etc/sudoers
-
-# sed '/^anothervalue=.*/a after=me' test.txt
-# sudo sed -Ei 's/^#(%saptb.*SU_ROOT.*)/\1/' /etc/sudoers
-
-# s/         # Substitution
-# ^          # Match the start of the line
-# (          # Capture the following
-# %saptb     # Followed by %saptb
-# .*         # Followed by anything
-# SU_ROOT    # Followed by SU_ROOT
-# .*         # Followed by anything
-# )          # Close capture
-# /          # Replace with 
-# #          # A hash 
-# \1         # Followed by the captured line
-
-
-# mykey=one
-# anothervalue=two
-# after=me
-# lastvalue=three
+#sudo sed -i '/User privilege specification/a gkiraly  ALL\=\(ALL\) ALL' /etc/sudoers
 
 # Install ssh-server
-#apt-get install openssh-server
+apt-get install openssh-server
 
 # Restart ssh-server
-#systemctl restart sshd.service
+systemctl restart sshd.service
 
 # nano /etc/ssh/sshd_config
+
+value=$( sudo systemctl status sshd.service | grep -ic "Active: active (running)" )
+
+echo "DEBUG $value"
+
+#if [ $value -eq 1 ]
+#then
+#  echo "ACTIVE"
+#else
+#  echo "NOT ACTIVE"
+#fi
+
+while [ $value -eq 0 ]
+do
+	sleep 5
+	echo "Getting ssh daemon status ..."
+done
+
+echo "Modify sshd_config ..."
 
 # Modify sshd_config
 #sed -i 's/#\?\(PerminRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
 #sed -i 's/#\?\(MaxAuthTries\s*\).*$/\1 4/' /etc/ssh/sshd_config
 #sed -i 's/#\?\(MaxSessions\s*\).*$/\1 3/' /etc/ssh/sshd_config
-# sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
+#sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+echo "D O N E ! ! !"
 
